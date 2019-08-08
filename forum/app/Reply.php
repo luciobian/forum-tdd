2b6deef1b01eccfb54b18ae9d12eed28b08fd771
luciobian <lbiancardi91@gmail.com>
2019-08-07 23:38:10 -0300

    button favorite

--- forum/app/Reply.php
+++ forum/app/Reply.php
@@ -8,8 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     protected $guarded = [];
+
+
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function favorites()
+    {
+        return $this->morphMany(Favorite::class, 'favorited');
+    }
+    public function favorite()
+    {
+        $atributte = ['user_id'=>auth()->id()];
+        if(!$this->favorites()->where($atributte)->exists()){
+            return $this->favorites()->create($atributte);
+        }
     }
 }
---
+++ forum/tests/Feature/FavoritesTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class FavoritesTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function guestCanNotFavoriteAnything()
+    {
+        $this->withExceptionHandling() 
+            ->post("/replies/1/favorites")
+            ->assertRedirect('/login');
+    }
+    
+    /** @test */
+    function anAutenticatedUserCanFavoriteAnyReply()
+    {
+        $this->signIn();
+        $reply = create("App\Reply");
+
+        $this->post("/replies/".$reply->id."/favorites");
+        
+        $this->assertCount(1, $reply->favorites);
+    }
+
+    /** @test */
+    function anAuthenticatedUserCanOnlyFavoriteAReplyOnce()
+    {
+        $this->signIn();
+        $reply = create("App\Reply");
+
+        try {
+            //code...
+            $this->post("/replies/".$reply->id."/favorites");
+            $this->post("/replies/".$reply->id."/favorites");
+        } catch (\Exception $e) {
+            $this->fail('Did not expect to insert the same record set twice.');
+        }
+
+
+        $this->assertCount(1, $reply->favorites);
+    }
+}
