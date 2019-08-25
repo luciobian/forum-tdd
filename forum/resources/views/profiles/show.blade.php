7e9dad188261220d431eac63a96d588de6298ec3
luciobian <lbiancardi91@gmail.com>
2019-08-25 16:42:28 -0300

    link my profile in nav menu

--- forum/resources/views/profiles/show.blade.php
+++ forum/resources/views/profiles/show.blade.php
@@ -1,35 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="container">
+<div class="container">
+    <div class="row">
+
+        <div class="col-md-8 offset-md-2">
 
             <div class="pb-2 mt-4 mb-2 border-bottom">
                 <h1>
-                    {{ $profileUser->name }}    
+                    {{ $profileUser->name }}
                 </h1>
                 <small> Since: {{ $profileUser->created_at->diffForHumans() }} </small>
             </div>
             @foreach ($threads as $thread)
-                <div class="card mb-3">               
-                    <div class="card-header">
-                        <div class="level">
-                            <div class="flex">
-                                <a href=""> {{$thread->creator->name}}</a> posted: 
-                                {{$thread->title}}
-                            </div>
-                            <span>
-                                {{$thread->created_at->diffForHumans()}}
-                            </span>
+            <div class="card mb-3">
+                <div class="card-header">
+                    <div class="level">
+                        <div class="flex">
+                            <a href=""> 
+                                {{$thread->creator->name}}
+                            </a> posted:
+                            {{$thread->title}}
                         </div>
-                        </div>
-    
-                    <div class="card-body">
-                        {{$thread->body}}
+                        <span>
+                            {{$thread->created_at->diffForHumans()}}
+                        </span>
                     </div>
                 </div>
+
+                <div class="card-body">
+                    {{$thread->body}}
+                </div>
+            </div>
             @endforeach
             {{ $threads->links() }}
+
         </div>
+
+    </div>
+
+</div>
 
 
 @endsection
